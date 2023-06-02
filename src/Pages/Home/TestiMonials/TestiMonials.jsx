0cf9606abe8e71cd3a7c1acc2f62a8ca507841c3
Rakibul-Islam01 <iamrakibulislam01@gmail.com>
2023-06-02 09:00:28 +0600

    reviews section added

--- src/Pages/Home/TestiMonials/TestiMonials.jsx
+++ src/Pages/Home/TestiMonials/TestiMonials.jsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
 
 const TestiMonials = () => {
+    const [reviews, setReviews] = useState([])
+
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
     return (
         <div>
             <SectionTitle
-            subHeading={"---What Our Clients Say---"}
-            heading={"TESTIMONIALS"}
+                subHeading={"---What Our Clients Say---"}
+                heading={"TESTIMONIALS"}
             >
             </SectionTitle>
+
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper my-20">
+
+                {
+                    reviews.map(review => <SwiperSlide key={review._id}>
+                        <div className='flex flex-col justify-center items-center my-12 mx-24 gap-3'>
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <p className='mt-4'>{review.details}</p>
+                            <p className='text-2xl text-yellow-700'>{review.name}</p>
+                        </div>
+
+                    </SwiperSlide>)
+                }
+            </Swiper>
         </div>
     );
 };
